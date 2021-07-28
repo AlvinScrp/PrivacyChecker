8ba79b16e80f818855272065cdc969470a948df3
mawenqiang <351033166@qq.com>
2021-07-28 14:14:04 +0800

    plugin0.0.2

--- buildSrc/src/main/groovy/com/a/plugin/test/PrivacyCheckPlugin.groovy
+++ buildSrc/src/main/groovy/com/a/plugin/test/PrivacyCheckPlugin.groovy
@@ -1,5 +1,6 @@
-package com.a.plugin
+package com.a.plugin.test
 
+import com.a.plugin.PrivacyCheckTransformRob
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,7 +8,7 @@ class PrivacyCheckPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println "this is my custom plugin PrivacyCheckPlugin in buildSrc"
+        println "this is my custom plugin PrivacyCheckPlugin in buildSrc test"
 
 //        project.android.registerTransform(new PrivacyCheckTransform(project))
         project.android.registerTransform(new PrivacyCheckTransformRob(project))
