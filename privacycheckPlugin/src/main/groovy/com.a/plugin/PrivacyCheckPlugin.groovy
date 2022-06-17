68e1cb541e56ceb8fd73b73e0ec822be41bdd377
canglong <351033166@qq.com>
2022-06-17 13:53:34 +0800

    提交

--- privacycheckPlugin/src/main/groovy/com.a/plugin/PrivacyCheckPlugin.groovy
+++ privacycheckPlugin/src/main/groovy/com.a/plugin/PrivacyCheckPlugin.groovy
@@ -1,5 +1,9 @@
 package com.a.plugin
 
+
+import com.a.plugin.PrivacyCheckTransformRob
+import com.a.plugin.PrivacyExtension
+import com.a.plugin.privacycheck.PrivacyConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,9 +11,50 @@ class PrivacyCheckPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println "this is my custom plugin PrivacyCheckPlugin inplugin"
+        println "this is my custom plugin PrivacyCheckPlugin in buildSrc test"
+        project.extensions.create('privacyExtension', PrivacyExtension)
+        project.afterEvaluate {
+            println '配置信息======='
+            Set<String> privacySet = project['privacyExtension'].privacySet
+            if (privacySet != null && !privacySet.isEmpty()) {
+                for (item in privacySet) {
+                    println '方法名=' + item
+                }
+//                PrivacyConfig.methodSet.clear()
+                PrivacyConfig.methodSet.addAll(privacySet)
+            }
 
-//        project.android.registerTransform(new PrivacyCheckTransform(project))
+            Set<String> fieldSet = project['privacyExtension'].fieldSet
+            if (fieldSet != null && !fieldSet.isEmpty()) {
+//              PrivacyConfig.fieldSet.clear()
+                PrivacyConfig.fieldSet.addAll(project['privacyExtension'].fieldSet)
+            }
+            Set<String> ignoreClass = project['privacyExtension'].ignoreClass
+            if (ignoreClass != null && !ignoreClass.isEmpty()) {
+                PrivacyConfig.ignoreClasses.clear()
+                PrivacyConfig.ignoreClasses.addAll(ignoreClass)
+            }
+
+//            if (project['privacyExtension'].statementAllowBool != null) {
+//                PrivacyConfig.Statement_Allow_bool = project['privacyExtension'].statementAllowBool
+//                println 'Statement_Allow_bool=' + PrivacyConfig.Statement_Allow_bool
+//            }
+
+            if (project['privacyExtension'].statementRejectValue != null) {
+                PrivacyConfig.Statement_Reject_Method = project['privacyExtension'].statementRejectValue
+                println 'Statement_Reject_Value=' + PrivacyConfig.Statement_Reject_Method
+            }
+//            if (project['privacyExtension'].logTagAllow != null) {
+//                PrivacyConfig.Log_tag_Allow = project['privacyExtension'].logTagAllow
+//                println 'Log_tag_Allow=' + PrivacyConfig.Log_tag_Allow
+//            }
+//            if (project['privacyExtension'].logTagReject != null) {
+//                PrivacyConfig.Log_tag_Reject = project['privacyExtension'].logTagReject
+//                println 'Log_tag_Reject=' + PrivacyConfig.Log_tag_Reject
+//            }
+
+
+        }
         project.android.registerTransform(new PrivacyCheckTransformRob(project))
     }
 
--- privacycheckPlugin/src/main/java/com/a/plugin/privacycheck/InjectMethodProxy.java
+++ privacycheckPlugin/src/main/java/com/a/plugin/privacycheck/InjectMethodProxy.java
@@ -9,10 +9,19 @@ import javassist.runtime.Desc;
 public class InjectMethodProxy {
 
     public static void execute(MethodCall m) throws CannotCompileException {
-        System.out.println(m.getSignature());
-//        System.out.println(Arrays.toString(Desc.getParams(m.getSignature())));
-        String replace = "{  $_ =($r)( com.a.privacychecker.MainApp.privacyVisitProxy(\""+ m.getClassName()+"\",\""+m.getMethodName()+"\", $0,$sig, $args)); }";
+        String mLongName = m.getClassName() + "." + m.getMethodName();
+        StringBuilder sb=new StringBuilder();
+        sb.append("{ ");
+        sb.append("   android.util.Log.d(\"alvin\",android.util.Log.getStackTraceString(new Throwable(\"" + mLongName + "\"))); ");
+        sb.append( "  $_ =($r)( com.a.privacychecker.MainApp.privacyVisitProxy(\""+ m.getClassName()+"\",\""+m.getMethodName()+"\", $0,$sig, $args)); ");
+        sb.append("}");
+        String replace =sb.toString();
         m.replace(replace);
+
+
+
+//        String replace = "{  $_ =($r)( com.a.privacychecker.MainApp.privacyVisitProxy(\""+ m.getClassName()+"\",\""+m.getMethodName()+"\", $0,$sig, $args)); }";
+//        m.replace(replace);
 
 
     }
